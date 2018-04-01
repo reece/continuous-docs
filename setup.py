a5252560379e39229fc3d3257f022239504a0210
Reece Hart <reecehart@gmail.com>
2018-03-31 17:02:07 -0700

    use setuptools_scm

--- setup.py
+++ setup.py
@@ -4,10 +4,14 @@ from setuptools import setup, find_packages
 
 
 setup(name='docpkg',
-      version='0.1.0',
+      use_scm_version = True,
       author='Ian Good',
       author_email='icgood@gmail.com',
-      packages=find_packages())
+      packages=find_packages(),
+      setup_requires=[
+          "setuptools_scm"
+          ]
+)
 
 
 # vim:et:fdm=marker:sts=4:sw=4:ts=4
